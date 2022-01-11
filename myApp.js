831d07db977e2c59f814f9e43fb856b54499fdc3
Charles Shelton <vbsmbb@gmail.com>
2022-01-11 07:50:29 -0600

    Added .env file to MyApp and JSON msg

--- myApp.js
+++ myApp.js
@@ -1,6 +1,27 @@
 var express = require('express');
 var app = express();
+require('dotenv').config();
 
-console.log("Hello WOrld");
+var pubPath = __dirname + '/public';
+app.use("/public", express.static(pubPath));
 
- module.exports = app;
+var vwPath = __dirname + '/views/index.html';
+app.get('/', (req,res) => {
+  res.sendFile(vwPath)
+});
+
+var msg;
+if (process.env.MESSAGE_STYLE === 'uppercase') {
+      msg = {"message": "HELLO JSON"}
+} else {
+      msg = {"message": "Hello json"}
+}
+console.log("msg:", msg);
+app.get('/json', (req, res) => {
+  res.json(msg)
+});
+
+  
+console.log("Hello World");
+
+module.exports = app;
